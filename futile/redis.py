a41f5d05454f8c4299fecec74d8e0e435856406c
Yifei Kong <kongyifei@gmail.com>
2018-10-17 09:30:45 +0800

    add optional conf args

--- futile/redis.py
+++ futile/redis.py
@@ -3,7 +3,7 @@ import redis
 from .consul import lookup_service
 
 
-def make_redis_client() -> redis.StrictRedis:
+def make_redis_client(conf=None) -> redis.StrictRedis:
     # addresses = lookup_service('inf.db.redis')
     # ip, port = addresses[0]
     ip = os.environ.get('REDIS_IP')
@@ -11,7 +11,7 @@ def make_redis_client() -> redis.StrictRedis:
     return redis.StrictRedis(ip, port)
 
 
-def make_pika_client() -> redis.StrictRedis:
+def make_pika_client(conf=None) -> redis.StrictRedis:
     # addresses = lookup_service('inf.db.pika')
     # ip, port = addresses[0]
     ip = os.environ.get('PIKA_IP')

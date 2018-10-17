461057e2219bf23357b2cfd25a6f37735e5fe8de
Yifei Kong <kongyifei@gmail.com>
2018-10-17 09:30:19 +0800

    using black

--- futile/grpc/executor.py
+++ futile/grpc/executor.py
@@ -23,24 +23,25 @@ def _loop_mgr(loop: asyncio.AbstractEventLoop):
 
 
 class AsyncioExecutor(futures.Executor):
-
     def __init__(self, *, loop=None):
 
         super().__init__()
         self._shutdown = False
         self._loop = loop or asyncio.get_event_loop()
-        self._thread = threading.Thread(target=_loop_mgr, args=(self._loop,),
-                                        daemon=True)
+        self._thread = threading.Thread(
+            target=_loop_mgr, args=(self._loop,), daemon=True
+        )
         self._thread.start()
 
     def submit(self, fn, *args, **kwargs):
 
         if self._shutdown:
-            raise RuntimeError('Cannot schedule new futures after shutdown')
+            raise RuntimeError("Cannot schedule new futures after shutdown")
 
         if not self._loop.is_running():
-            raise RuntimeError("Loop must be started before any function can "
-                               "be submitted")
+            raise RuntimeError(
+                "Loop must be started before any function can " "be submitted"
+            )
 
         if inspect.iscoroutinefunction(fn):
             coro = fn(*args, **kwargs)
@@ -67,11 +68,16 @@ async def _call_behavior(rpc_event, state, behavior, argument, request_deseriali
     except Exception as e:  # pylint: disable=broad-except
         with state.condition:
             if e not in state.rpc_errors:
-                details = 'Exception calling application: {}'.format(e)
+                details = "Exception calling application: {}".format(e)
                 _server.logging.exception(details)
-                _server._abort(state, rpc_event.operation_call,
-                       _server.cygrpc.StatusCode.unknown, _server._common.encode(details))
+                _server._abort(
+                    state,
+                    rpc_event.operation_call,
+                    _server.cygrpc.StatusCode.unknown,
+                    _server._common.encode(details),
+                )
         return None, False
+
 
 async def _take_response_from_response_iterator(rpc_event, state, response_iterator):
     try:
@@ -81,47 +87,71 @@ async def _take_response_from_response_iterator(rpc_event, state, response_itera
     except Exception as e:  # pylint: disable=broad-except
         with state.condition:
             if e not in state.rpc_errors:
-                details = 'Exception iterating responses: {}'.format(e)
+                details = "Exception iterating responses: {}".format(e)
                 _server.logging.exception(details)
-                _server._abort(state, rpc_event.operation_call,
-                       _server.cygrpc.StatusCode.unknown, _server._common.encode(details))
+                _server._abort(
+                    state,
+                    rpc_event.operation_call,
+                    _server.cygrpc.StatusCode.unknown,
+                    _server._common.encode(details),
+                )
         return None, False
 
-async def _unary_response_in_pool(rpc_event, state, behavior, argument_thunk,
-                                  request_deserializer, response_serializer):
+
+async def _unary_response_in_pool(
+    rpc_event,
+    state,
+    behavior,
+    argument_thunk,
+    request_deserializer,
+    response_serializer,
+):
     argument = argument_thunk()
     if argument is not None:
-        response, proceed = await _call_behavior(rpc_event, state, behavior,
-                                                 argument, request_deserializer)
+        response, proceed = await _call_behavior(
+            rpc_event, state, behavior, argument, request_deserializer
+        )
         if proceed:
             serialized_response = _server._serialize_response(
-                rpc_event, state, response, response_serializer)
+                rpc_event, state, response, response_serializer
+            )
             if serialized_response is not None:
                 _server._status(rpc_event, state, serialized_response)
 
-async def _stream_response_in_pool(rpc_event, state, behavior, argument_thunk,
-                                   request_deserializer, response_serializer):
+
+async def _stream_response_in_pool(
+    rpc_event,
+    state,
+    behavior,
+    argument_thunk,
+    request_deserializer,
+    response_serializer,
+):
     argument = argument_thunk()
     if argument is not None:
         # Notice this calls the normal `_call_behavior` not the awaitable version.
         response_iterator, proceed = _server._call_behavior(
-            rpc_event, state, behavior, argument, request_deserializer)
+            rpc_event, state, behavior, argument, request_deserializer
+        )
         if proceed:
             while True:
                 response, proceed = await _take_response_from_response_iterator(
-                    rpc_event, state, response_iterator)
+                    rpc_event, state, response_iterator
+                )
                 if proceed:
                     if response is None:
                         _server._status(rpc_event, state, None)
                         break
                     else:
                         serialized_response = _server._serialize_response(
-                            rpc_event, state, response, response_serializer)
+                            rpc_event, state, response, response_serializer
+                        )
                         print(response)
                         if serialized_response is not None:
                             print("Serialized Correctly")
-                            proceed = _server._send_response(rpc_event, state,
-                                                     serialized_response)
+                            proceed = _server._send_response(
+                                rpc_event, state, serialized_response
+                            )
                             if not proceed:
                                 break
                         else:
@@ -129,11 +159,13 @@ async def _stream_response_in_pool(rpc_event, state, behavior, argument_thunk,
                 else:
                     break
 
+
 _server._unary_response_in_pool = _unary_response_in_pool
 _server._stream_response_in_pool = _stream_response_in_pool
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import grpc
+
     server = grpc.server(AsyncioExecutor())
     # Add Servicer and Start Server Here

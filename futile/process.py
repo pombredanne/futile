772f4cce98fb2edfa68328df2099fb3d93b65d0a
Yifei Kong <kongyifei@gmail.com>
2018-10-25 09:55:27 +0800

    add process

---
+++ futile/process.py
@@ -0,0 +1,23 @@
+import prctl
+import signal
+import sys
+import os
+
+
+def run_process(target, *, auto_quit=True):
+    try:
+        pid = os.fork()
+    except OSError:
+        print("unable to fork")
+        sys.exit(-1)
+    if pid:
+        return
+    else:
+        if auto_quit:
+            prctl.set_pdeathsig(signal.SIGTERM)
+        try:
+            target()
+        except KeyboardInterrupt:
+            pass
+        sys.exit()
+

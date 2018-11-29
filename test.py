ec09312ddd54ef2ba06110b7d62fb833ac7b931e
Yifei Kong <kongyifei@gmail.com>
2018-11-29 16:03:40 +0800

    removed useless files

--- test.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-
-import requests
-from futile.html import build_doc
-import lxml.html
-
-doc = build_doc(requests.get('http://finance.sina.com.cn/stock/jsy/2017-11-14/doc-ifynstfh7937123.shtml').content)
-print(lxml.html.tostring(doc, encoding='unicode', pretty_print=True))

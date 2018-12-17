ce2c47ec34fbd587580ac7e94abc10b37ae164b8
Yifei Kong <kongyifei@gmail.com>
2018-12-17 15:48:57 +0800

    add nomad

---
+++ futile/nomad.py
@@ -0,0 +1,40 @@
+import os
+import json
+import requests
+
+
+class NomadException(Exception):
+    pass
+
+
+class Nomad:
+    def __init__(self, host="127.0.0.1", port=4646, timeout=5):
+        if host != "127.0.0.1" or port != 4646:
+            self._address = f"http://{host}:{port}"
+        else:
+            self._address = os.getenv("NOMAD_ADDR", f"http://{host}:{port}")
+        self._timeout = timeout
+        self._session = requests.Session()
+
+    def __getattr__(self, attr):
+        method, *endpoints = attr.split("_")
+
+        def fn(id=None, **kwargs):
+            url = f'{self._address}/v1/{"/".join(endpoints)}'
+            if id is not None:
+                url += f'/{id}'
+            try:
+                if method == "get":
+                    rsp = self._session.request(
+                        method=method, url=url, params=kwargs, timeout=self._timeout
+                    )
+                else:
+                    rsp = self._session.request(
+                        method=method, url=url, json=kwargs, timeout=self._timeout
+                    )
+            except Exception as e:
+                raise NomadException from e
+            print(rsp.text)
+            return rsp.json()
+
+        return fn

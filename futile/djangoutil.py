9996ef9b6a7100dadb55a527e1fba32aef681276
Yifei Kong <kongyifei@gmail.com>
2019-01-04 16:13:02 +0800

    add djangoutil

---
+++ futile/djangoutil.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+from django.db import close_old_connections
+from django.db.utils import OperationalError
+
+
+def ensure_db_connection(fn):
+    @wraps
+    def wrapped(*args, **kwargs):
+        try:
+            fn(*args, **kwargs)
+        except OperationalError:
+            close_old_connections()
+            fn(*args, **kwargs)

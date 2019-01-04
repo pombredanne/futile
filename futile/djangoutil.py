232977da75742a66a50aa5f1bab76b0b4d093938
Yifei Kong <kongyifei@gmail.com>
2019-01-04 16:54:13 +0800

    fix django util

--- futile/djangoutil.py
+++ futile/djangoutil.py
@@ -1,3 +1,5 @@
+import logging
+
 from functools import wraps
 
 from django.db import close_old_connections
@@ -5,11 +7,23 @@ from django.db.utils import OperationalError
 
 
 def ensure_db_connection(fn):
-    @wraps
+    @wraps(fn)
     def wrapped(*args, **kwargs):
         try:
-            fn(*args, **kwargs)
+            return fn(*args, **kwargs)
         except OperationalError:
+            logging.info("django db disconnected")
             close_old_connections()
-            fn(*args, **kwargs)
+            return fn(*args, **kwargs)
+
     return wrapped
+
+
+if __name__ == "__main__":
+
+    @ensure_db_connection
+    def hello():
+        print("hello")
+        raise OperationalError
+
+    hello()

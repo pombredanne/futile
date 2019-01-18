fff6b6c65bbf8e1b47e85ce97b1807015ea9d48c
Yifei Kong <kongyifei@gmail.com>
2019-01-19 00:01:26 +0800

    metrics2: support base tags

--- futile/metrics2.py
+++ futile/metrics2.py
@@ -4,41 +4,52 @@ import statsd
 _emitter = None
 
 
-def fmttags(measurement, tags):
+def fmttags(measurement, tags, base_tags=None):
     """
     >>> fmttags("foo", dict(hello="world", true="false"))
     'foo,hello=world,true=false'
+    >>> fmttags("foo", dict(hello="world", true="false"), dict(db="spider"))
+    'foo,hello=world,true=false,db=spider'
     """
-    if tags is None:
+    if not tags and not base_tags:
         return measurement
+    if base_tags:
+        tags = {**tags, **base_tags}  # NOTE 不能直接使用 update, 否则会更改 tags
     tagstr = ",".join([f"{k}={v}" for k, v in tags.items()])
     return ",".join([measurement, tagstr])
 
 
 class MetricsEmitter:
-    def __init__(self, host="localhost", port=8125, prefix=None):
+    def __init__(self, host="localhost", port=8125, prefix=None, tags=None):
+        """
+        Args:
+            prefix: global measurement prefix
+            tags: global tag dict that will be added to each metric point,
+                such as {"db": "spider"}
+        """
         self._client = statsd.StatsClient(host, port, prefix)
+        self._tags = tags
 
     def emit_counter(self, key, value, *, tags=None, rate=1):
-        key = fmttags(key, tags)
+        key = fmttags(key, tags, self._tags)
         self._client.incr(key, value, rate=rate)
 
     def emit_timer(self, key, value, *, tags=None, rate=1):
-        key = fmttags(key, tags)
+        key = fmttags(key, tags, self._tags)
         self._client.timing(key, value, rate=rate)
 
     def emit_store(self, key, value, *, tags=None, rate=1, delta=False):
-        key = fmttags(key, tags)
+        key = fmttags(key, tags, self._tags)
         self._client.gauge(key, value, rate=rate, delta=delta)
 
 
-def init(*, host=None, port=None, prefix=None):
+def init(*, host=None, port=None, prefix=None, tags=None):
     global _emitter
     if host is None:
         host = os.getenv("STATSD_HOST") or "localhost"
     if port is None:
         port = os.getenv("STATSD_PORT") or 8125
-    _emitter = MetricsEmitter(host, port, prefix)
+    _emitter = MetricsEmitter(host, port, prefix, tags)
 
 
 def emit_counter(key, value, *, tags=None, rate=1):

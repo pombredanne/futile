e09cef5aaae4d9ba99bc6f8eddf839c49f4d3e2c
Yifei Kong <kongyifei@gmail.com>
2019-02-03 16:03:06 +0800

    consul: use black

--- futile/consul.py
+++ futile/consul.py
@@ -7,14 +7,14 @@ from futile.net import get_local_ip
 def lookup_service(service_name):
     consul = Consul()
     _, services = consul.catalog.service(service_name)
-    endpoints = [(s['Address'], s['ServicePort']) for s in services]
+    endpoints = [(s["Address"], s["ServicePort"]) for s in services]
     return endpoints
 
 
 def lookup_kv(key, default=None):
     consul = Consul()
     _, data = consul.kv.get(key)
-    return data.get('Value', default)
+    return data.get("Value", default)
 
 
 def register_service(service_name: str, *, address: str = None, port: int = None):
